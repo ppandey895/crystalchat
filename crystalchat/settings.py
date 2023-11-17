429570bdfe98bd0f405d9d24a4e4512f6e1623df
pramod <ppandey200203@gmail.com>
2023-11-17 19:49:02 +0530

    .

--- crystalchat/settings.py
+++ crystalchat/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'crystalchat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'acg5ceFB4b6D3db2beBeDA53DgD*eFdG',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '24692',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'acg5ceFB4b6D3db2beBeDA53DgD*eFdG',
+#         'HOST': 'monorail.proxy.rlwy.net',
+#         'PORT': '24692',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
